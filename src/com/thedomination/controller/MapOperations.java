cee931963b73f33e449c616402a983bd61a18393
pritamkumar24 <pritamdabby@gmail.com>
2019-10-08 16:30:23 -0400

    MapOperation File edited

--- src/com/thedomination/controller/MapOperations.java
+++ src/com/thedomination/controller/MapOperations.java
@@ -15,23 +15,14 @@ import com.thedomination.utilities.MapReader;
  */
 public class MapOperations {
 
-	/** The conquest map name. */
-	private String conquestMapName = "Default";
+	private String DefaultName = "NewMapFile";
 	
-	//edit 2019
+	//2019
 	private MapReader mapReader;
 	
-	/** The continents list. */
 	private ArrayList<ContinentModel> continentsList;
 	
-	/** The country list. */
 	private ArrayList<CountryModel> countryList;
-
-	/** The value error flag. */
-	private boolean valErrorFlag = false;
-
-	/** The error message. */
-	private String errorMsg = "Map is invalid";
 	
 	//2019
 	private static MapOperations UniqueInstance;
@@ -43,123 +34,42 @@ public class MapOperations {
 		return MapOperations.UniqueInstance;
 	}
 
-	/**
-	 * MapHierarchyModel Constructor Instantiates a new map hierarchy model.
-	 */
+
 	public MapOperations() {
 		this.continentsList = new ArrayList<>();
 		this.countryList = new ArrayList<>();
 	}
 
-	/**
-	 * MapHierarchyModel Parameterized Constructor Instantiates a new map hierarchy
-	 * model.
-	 * 
-	 * @param conquestMapName the conquest map name
-	 * @param totalCountries  the total countries
-	 */
 	public MapOperations(String conquestMapName, int totalCountries) {
-		this.conquestMapName = conquestMapName;
+		this.DefaultName = conquestMapName;
 		this.continentsList = new ArrayList<ContinentModel>();
 		this.countryList = new ArrayList<>();
 	}
 
-	/**
-	 * Gets the conquest map name. Getter function to get the map name
-	 * 
-	 * @return the conquestMapName
-	 */
-	public String getConquestMapName() {
-		return conquestMapName;
+	public String getDefaultName() {
+		return DefaultName;
 	}
 
-	/**
-	 * Sets the conquest map name. Setter function to set the map name
-	 * 
-	 * @param conquestMapName the conquestMapName to set
-	 */
-	public void setConquestMapName(String conquestMapName) {
-		this.conquestMapName = conquestMapName;
+	public void setDefaultName(String defaultName) {
+		DefaultName = defaultName;
 	}
 
-	/**
-	 * Gets the continents list.
-	 * 
-	 * @return the continentsList
-	 */
 	public ArrayList<ContinentModel> getContinentsList() {
 		return continentsList;
 	}
 
-	/**
-	 * Sets the continents list.
-	 * 
-	 * @param continentsList the continentsList to set
-	 */
 	public void setContinentsList(ArrayList<ContinentModel> continentsList) {
 		this.continentsList = continentsList;
 	}
 
-	/**
-	 * Gets the country list.
-	 * 
-	 * @return the countryList
-	 */
 	public ArrayList<CountryModel> getCountryList() {
 		return countryList;
 	}
 
-	/**
-	 * Sets the country list.
-	 * 
-	 * @param countryModels the countryList to set
-	 */
 	public void setCountryList(ArrayList<CountryModel> countryModels) {
 		this.countryList = countryModels;
 	}
 
-	/**
-	 * Checks if is value error flag.
-	 * 
-	 * @return the valErrorFlag
-	 */
-	public boolean isValErrorFlag() {
-		return valErrorFlag;
-	}
-
-	/**
-	 * Sets the value error flag.
-	 * 
-	 * @param valErrorFlag the valErrorFlag to set
-	 */
-	public void setValErrorFlag(boolean valErrorFlag) {
-		this.valErrorFlag = valErrorFlag;
-	}
-
-	/**
-	 * Gets the error message.
-	 * 
-	 * @return the errorMsg
-	 */
-	public String getErrorMsg() {
-		return errorMsg;
-	}
-
-	/**
-	 * Sets the error message.
-	 * 
-	 * @param errorMsg the errorMsg to set
-	 */
-	public void setErrorMsg(String errorMsg) {
-		this.errorMsg = errorMsg;
-	}
-
-	/**
-	 * Method to search the continent according to the continent's name.
-	 * 
-	 * @param continentName continent's name
-	 * @return continent that found or null if not exits
-	 */
 	public ContinentModel searchContinent(String continentName) {
 		for (ContinentModel loopContinent : getContinentsList()) {
 			if (loopContinent.getContinentName().equalsIgnoreCase(continentName)) {
@@ -169,42 +79,24 @@ public class MapOperations {
 		return null;
 	}
 
-	/**
-	 * Method to add a new continent.
-	 * 
-	 * @param continentName name of the new continent
-	 * @return the string
-	 */
+
 	public void  addContinent(String continentName,int continentValue) {
-//		if (searchContinent(continentName) != null) {
-//			return "Continent <" + continentName + "> already exists";
-//		}
+
 		if (continentName != null && !continentName.trim().isEmpty() && continentValue>0 ) {
 			ContinentModel newContinent = new ContinentModel(continentName,continentValue);
 			getContinentsList().add(newContinent);
-			//System.out.println(newContinent.toString());
-			//System.out.println("hello****************************");
-			//System.out.println(mapReader.continentModels);
-			//mapReader.continentModels.add(newContinent);
-			
+
 		}
 		for(ContinentModel temp1:continentsList)
 		{
-		System.out.println(temp1);}
-		//return "";	
+		System.out.println(temp1);}	
 	}
 
-	/**
-	 * Method to delete a continent.
-	 * 
-	 * @param continentName name of the continent want to delete
-	 * @return succeed or failed error message
-	 */
 	public String deleteContinent(String continentName) {
 		ContinentModel deleteContinent = searchContinent(continentName);
 		if (deleteContinent != null) {
 			if (deleteContinent.getCountriesList().size() > 0) {
-				return "Continent '" + continentName + "' is not empty, Please delete or move all the countries in it.";
+				return "Continent '" + continentName + "' is not there";
 			}
 			getContinentsList().remove(deleteContinent);
 			deleteContinent = null;
@@ -215,59 +107,11 @@ public class MapOperations {
 		return "";
 	}
 
-	/**
-	 * Method to rename a continent.
-	 * 
-	 * @param continentName    name of the continent want to rename
-	 * @param newContinentName new name of the continent
-	 * @return succeed or failed error message
-	 */
-	public String renameContinent(String continentName, String newContinentName) {
-		ContinentModel continent = searchContinent(continentName);
-		if (continent == null) {
-			return "Continent  '" + continentName + "'  you want to change does not exists";
-		}
-		if (searchContinent(newContinentName) != null) {
-			return "Continent '" + newContinentName + "' already exits";
-		} else {
-			continent.setContinentName(newContinentName);
-		}
-		return "";
-	}
 
-	/**
-	 * Method to rename a continent.
-	 * 
-	 * @param countryName     name of the continent want to rename
-	 * @param newCountrytName new name of the continent
-	 * @return succeed or failed error message
-	 */
-	public String renameCountry(String countryName, String newCountrytName) {
-		CountryModel country = searchCountry(countryName);
-		if (country == null) {
-			return "Country  '" + countryName + "'  you want to change does not exists";
-		}
-		if (searchCountry(newCountrytName) != null) {
-			return "Country '" + newCountrytName + "' already exits";
-		} else {
-			country.setCountryName(newCountrytName);
-		}
-		return "";
-	}
 
-	/**
-	 * Method to find the country according to the country's name.
-	 * 
-	 * @param countryName country's name
-	 * @return country that found or null if not exits
-	 */
 	public CountryModel searchCountry(String countryName) {
 		CountryModel country = null;
-//		for (ContinentModel loopContinent : getContinentsList()) {
-//			country = loopContinent.searchCountry(countryName);
-//			if (country != null)
-//				return country;
-//		}
+
 		for(CountryModel tempCountry : countryList) {
 			if (tempCountry.getCountryName().equals(countryName))
 			return tempCountry;
@@ -275,21 +119,13 @@ public class MapOperations {
 		return null;
 	}
 	
-	/**
-	 * Method to add a new country to an existing continent.
-	 * 
-	 * @param countryName   name of the new country
-	 * @param continentName name of the existing continent that the new country
-	 *                      adding in
-	 * @return Error message
-	 */
 	public String addCountry(String countryName, String continentName) {
 		ContinentModel targetContinent = searchContinent(continentName);
 		if (targetContinent == null) {
-			return "Continent <" + continentName + "> does not exists";
+			return "Continent |" + continentName + "| does not exists";
 		}
 		if (searchCountry(countryName) != null) {
-			return "Country <" + countryName + "> already exists";
+			return "Country |" + countryName + "| already exists";
 		}
 		
 		CountryModel newCountry = new CountryModel(countryName, (countryList.size())+1, targetContinent);
@@ -348,12 +184,6 @@ public class MapOperations {
 	}
 
 	
-	/**
-	 * Method to delete a country.
-	 * 
-	 * @param countryName name of the country want to delete
-	 * @return succeed or failed error message
-	 */
 	public String deleteCountry(String countryName) {
 		CountryModel deleteCountry = searchCountry(countryName);
 		if (deleteCountry != null) {
@@ -367,52 +197,4 @@ public class MapOperations {
 		return "";
 	}
 
-	/**
-	 * Method to move a country.
-	 *
-	 * @param countryName   name of the country you want to move
-	 * @param continentName name of the continent to which you want to move
-	 * @return succeed or failed error message
-	 */
-	public String moveCountry(String countryName, String continentName) {
-		ContinentModel toContinent = searchContinent(continentName);
-		CountryModel moveCountry = searchCountry(countryName);
-		if (toContinent == null)
-			return "Continent  '" + continentName + "'  you want to move does not exists";
-		else if (continentName.equalsIgnoreCase(moveCountry.getBelongsTo().getContinentName()))
-			return "Continent name cannot be same as existing one";
-		else {
-			moveCountry.getBelongsTo().getCountriesList().remove(moveCountry);
-			toContinent.getCountriesList().add(moveCountry);
-			moveCountry.setBelongsTo(toContinent);
-		}
-		return "";
-	}
-
-
-//	/**
-//	 * Method to add a new country to an existing continent with neighbours.
-//	 * 
-//	 * @param countryName      name of the new country
-//	 * @param continentName    name of the existing continent that the new country
-//	 *                         adding in
-//	 * @param listOfNeighbours list of neighboring countries.
-//	 * @return Error message
-//	 */
-//	public String addCountry(String countryName, String continentName, ArrayList<String> listOfNeighbours) { //we have to change String to Integer
-//		ContinentModel targetContinent = searchContinent(continentName);
-//		if (targetContinent == null) {
-//			return "Continent <" + continentName + "> does not exists";
-//		}
-//		if (searchCountry(countryName) != null) {
-//			return "Country <" + countryName + "> already exists";
-//		}
-//
-//		totalCountries++;
-//		CountryModel newCountry = new CountryModel(countryName, targetContinent, listOfNeighbours);
-//		targetContinent.addCountry(newCountry);
-//		countryList.add(newCountry);
-//
-//		return "";
-//	}
 }
