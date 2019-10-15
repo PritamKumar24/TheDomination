c3774055f34ba5dc0533883707aef2015adb88e0
aditi <aditybhayana@gmail.com>
2019-10-15 00:07:16 -0400

    added new class for gameplayer

--- src/com/thedomination/model/PlayerModel.java
+++ src/com/thedomination/model/PlayerModel.java
@@ -1,118 +1,149 @@
 package com.thedomination.model;
 
-import java.util.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
-
-
-
+/**
+ * The Class PlayerModel.
+ *
+ * @author Aditi
+ * @version 1.0.0
+ */
 public class PlayerModel {
 
-  static int count=0;
- public static  int armiesToAssign;
-  public int armiesAvailableToAssign = -1;
+	/** The player country list. */
+	private List<CountryModel> playerCountryList;
 
+	/** The player name. */
+	private String playerName;
 
-  static ArrayList<String>playerNameList= new ArrayList<String>();
+	/** The no of army for player. */
+	private int noOfArmyInPlayer;
 
-  public static ArrayList<String> gamePlayer(String command)
-  {
-	  ArrayList<String>countList= new ArrayList<String>();
-
-	  String[] countPlayer=command.split(" ");
-
-	  for (int i = 1; i < countPlayer.length; i++) {
-
-		  countList.add(countPlayer[i]);
+	/**
+	 * PlayerModel Constructor Instantiates a new player model.
+	 *
+	 * @param playerName the player name
+	 */
+	public PlayerModel(String playerName) {
+		this.playerName = playerName;
+		this.playerCountryList = new ArrayList<>();
 	}
 
+	/**
+	 * PlayerModel Constructor Instantiates a new player model.
+	 *
+	 * @param playerName            the player name
+	 * @param countryModelArrayList array list of coutries of the player.
+	 */
 
-	  for (int i = 0; i < countList.size(); i++) {
-
-
-		  if(countList.get(i).equalsIgnoreCase("-add"))
-		  {
-
-				if(playerNameList!=null) {
-
-
-					if(!playerNameList.contains(countList.get(i+1))) {
-
-
-						playerNameList.add(countList.get(i+1));
-						System.out.println("Player " + countList.get(i+1)+ " has been added");
-
-						count++;
-					}else {
-						System.out.println("Player " + countList.get(i+1)+ " already exists");
-					}
-				}
-		  }
-
-		  else if(countList.get(i).equalsIgnoreCase("-remove"))
-		  {
-				if(playerNameList!=null) {
-					if(playerNameList.contains(countList.get(i+1))) {
-						playerNameList.remove(countList.get(i+1));
-						count--;
-						System.out.println("Player " + countList.get(i+1)+ " has been removed");
-					}else {
-						System.out.println("Player " + countList.get(i+1)+ " does not exist");
-					}
-				}
-		  }
+	public PlayerModel(String playerName, ArrayList<CountryModel> countryModelArrayList) {
+		this.playerName = playerName;
+		this.playerCountryList = new ArrayList<>();
+		this.playerCountryList.addAll(countryModelArrayList);
 	}
 
+	/**
+	 * noOfArmyinPlayer Method No of army for player.
+	 *
+	 * @param number the number
+	 */
 
+	public void setnoOfArmyInPlayer(int number) {
+		this.noOfArmyInPlayer = number;
+	}
 
+	/**
+	 * Reduce army for player.
+	 */
+	public void reduceArmyInPlayer() {
+		this.noOfArmyInPlayer--;
+	}
 
+	/**
+	 * Adds the army in player.
+	 */
+	public void addArmyInPlayer() {
+		this.noOfArmyInPlayer++;
+	}
 
-	return playerNameList;
+	public void addControlValueToNoOfArmy(int controlValue) {
+		this.noOfArmyInPlayer = this.noOfArmyInPlayer + controlValue;
+	}
 
+	/**
+	 * Gets the no of army for player.
+	 *
+	 * @return the no of army for player
+	 */
+	public int getnoOfArmyInPlayer() {
+		return noOfArmyInPlayer;
+	}
 
+	/**
+	 * getPlayerName Method Gets the player name.
+	 *
+	 * @return the player name
+	 */
+	public String getPlayerName() {
+		return playerName;
+	}
 
+	/**
+	 * setPlayerName Method Sets the player name.
+	 *
+	 * @param playerName the playerName to set
+	 */
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
 
-  }
+	/**
+	 * AddCountry Method Adds the country.
+	 *
+	 * @param countryName the country name
+	 */
+	public void AddCountry(CountryModel countryName) {
+		playerCountryList.add(countryName);
 
-  private HashMap<String, PlayerModel> players = new HashMap<>();
- // private HashMap<String, Country> countries = new HashMap<>();
-  //private HashMap<String, Continent> continents = new HashMap<>();
-  public void populateCountries(int playerno) {
+	}
 
+	/**
+	 * Removes the country.
+	 */
+	public void RemoveCountry() {
+		playerCountryList.removeAll(playerCountryList);
+	}
 
+	/**
+	 * Gets the player country list.
+	 *
+	 * @return the player country list
+	 */
+	public List<CountryModel> getPlayerCountryList() {
+		return playerCountryList;
+	}
 
-      switch (playerno) {
-          case 2:
-              armiesToAssign = 40;
-              System.out.println("Players have been assigned 40 armies.");
-              break;
-          case 3:
-              armiesToAssign = 35;
-              System.out.println("Players have been assigned 40 armies.");
-              break;
-          case 4:
-              armiesToAssign = 30;
-              System.out.println("Players have been assigned 30 armies.");
-              break;
-          case 5:
-              armiesToAssign = 25;
-              System.out.println("Players have been assigned 25 armies.");
-              break;
-          case 6:
-              armiesToAssign = 20;
-              System.out.println("Players have been assigned 20 armies.");
-              break;
-      }
+	/**
+	 * Search a country by the country Name.
+	 *
+	 * @param countryName : Name of the country to be searched.
+	 * @return Returns the Country Model.
+	 */
 
+	public CountryModel searchCountry(String countryName) {
+		for (CountryModel country : playerCountryList) {
+			if (country.getCountryName().trim().equalsIgnoreCase(countryName.trim())) {
+				return country;
+			}
+		}
+		return null;
+	}
+	@Override
+	public String toString() {
+		return "PlayerModel [playerCountryList=" + playerCountryList + ", playerName=" + playerName
+				+ ", noOfArmyInPlayer=" + noOfArmyInPlayer + "]";
+	}
 
-  }
 }
-
-
-
-
-
-
-
-
-
