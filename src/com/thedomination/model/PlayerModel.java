6def4c58a80991546695aa135ffd093be59f0fdf
pritamkumar24 <pritamdabby@gmail.com>
2019-11-03 16:52:53 -0500

    Player Model updated for cards

--- src/com/thedomination/model/PlayerModel.java
+++ src/com/thedomination/model/PlayerModel.java
@@ -20,6 +20,10 @@ public class PlayerModel {
 
 	/** The no of army for player. */
 	private int noOfArmyInPlayer;
+	
+	private boolean hasWonTerritory;
+	
+	private ArrayList<CardsModel> cardList;
 
 	/**
 	 * PlayerModel Constructor Instantiates a new player model.
@@ -29,6 +33,7 @@ public class PlayerModel {
 	public PlayerModel(String playerName) {
 		this.playerName = playerName;
 		this.playerCountryList = new ArrayList<>();
+		cardList = new ArrayList<CardsModel>();
 	}
 
 	/**
@@ -99,6 +104,22 @@ public class PlayerModel {
 		this.playerName = playerName;
 	}
 
+	public boolean isHasWonTerritory() {
+		return hasWonTerritory;
+	}
+
+	public void setHasWonTerritory(boolean hasWonTerritory) {
+		this.hasWonTerritory = hasWonTerritory;
+	}
+
+	public ArrayList<CardsModel> getCardList() {
+		return cardList;
+	}
+
+	public void setCardList(ArrayList<CardsModel> cardList) {
+		this.cardList = cardList;
+	}
+
 	/**
 	 * AddCountry Method Adds the country.
 	 *
@@ -141,10 +162,71 @@ public class PlayerModel {
 		}
 		return null;
 	}
-	@Override
-	public String toString() {
-		return "PlayerModel [playerCountryList=" + playerCountryList + ", playerName=" + playerName
-				+ ", noOfArmyInPlayer=" + noOfArmyInPlayer + "]";
+	
+	public CardsModel searchCard(String cardName) {
+		for (CardsModel card : cardList) {
+			if (card.getCardName().trim().equalsIgnoreCase(cardName.trim())) {
+				return card;
+			}
+		}
+		return null;
 	}
+	
+	//method returns the card at given position
+	public CardsModel getInHandCard(int cardPosition) {
+		if(cardPosition>0 && cardPosition<=cardList.size()) {
+			int j = cardPosition-1;
+			CardsModel card = cardList.get(j);
+			if(card!=null) {
+				return card;
+			}
+		}
+		return null;
+	}
+	
+	public void addCard(CardsModel card) {
+			cardList.add(card);
+	}
+	
+//	public void removeCards(int firstPosition, int secondPosition, int thirdPosition) {
+//		System.out.println("During removal position "+ firstPosition+ " "+secondPosition+" "+thirdPosition);
+//		int[] removePositions = {firstPosition, secondPosition, thirdPosition}; 
+//		System.out.println("Array created for removal "+removePositions.toString());
+//		int j=0;
+//		for(int i=0; i<3;i++) {
+//			CardsModel card = getInHandCard(removePositions[j]);
+//			System.out.println("card which is going to be removed "+ card);
+//			cardList.remove(card);
+//			j++;
+//			System.out.println("value of j "+j);
+//		}
+//	}
+	
+	public void removeCards(CardsModel firstCard, CardsModel secondCard, CardsModel thirdCard) {
+		CardsModel[] removeCards = {firstCard, secondCard, thirdCard}; 
+		int j=0;
+		for(int i=0; i<3;i++) {
+			CardsModel card = searchCard(removeCards[j].getCardName());
+			cardList.remove(card);
+			j++;
+		}
+	}
+
+  public void showCards() {
+	  int i = 1;
+	  for(CardsModel tempCard: cardList) {
+		  System.out.print("At position "+i+" "+tempCard.getCardName());
+		  System.out.println();
+		  i++;
+	  }
+	  
+  }
+  
+	
+//	@Override
+//	public String toString() {
+//		return "PlayerModel [playerCountryList=" + playerCountryList + ", playerName=" + playerName
+//				+ ", noOfArmyInPlayer=" + noOfArmyInPlayer + "]";
+//	}
 
 }
