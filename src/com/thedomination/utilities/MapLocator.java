ce9338639cf936f2ed53a497194dc1fd3c1fdc4e
manni1067 <manpreet.singh1992@yahoo.com>
2019-11-21 16:57:23 -0500

    Adapter Pattern changes added

--- src/com/thedomination/utilities/MapLocator.java
+++ src/com/thedomination/utilities/MapLocator.java
@@ -1,5 +1,7 @@
 package com.thedomination.utilities;
 
+import com.thedomination.controller.MapOperations;
+
 /**
  * The Class MapLocator class to fetch the map from folder.
  *
@@ -7,14 +9,29 @@ package com.thedomination.utilities;
  * @version 1.0.0
  */
 public class MapLocator {
-/**
- * mapLocation method to fetch the map from given loaction.
- * @param mapName name of the map ile.
- */
-	
+	/**
+	 * mapLocation method to fetch the map from given loaction.
+	 *
+	 * @param mapName name of the map ile.
+	 */
+	//public static boolean conquestmap = false;
+
 	public static void mapLocation(String mapName) {
-		
-		MapReader mapReader = new MapReader(); 
-		mapReader.parseAndValidateMap(System.getProperty("user.dir") + "/resources/" + mapName);
+
+		MapReader mapReader = new MapReader();
+
+		ConquestMapReader conquestMapReader = new ConquestMapReader();
+
+		MapReader reader = new MapAdapter(conquestMapReader);
+
+		MapOperations op = reader.parseAndValidateMap(System.getProperty("user.dir") + "/resources/" + mapName);
+
+		if (op == null) {
+
+			MapOperations op1 = mapReader.parseAndValidateMap(System.getProperty("user.dir") + "/resources/" + mapName);
+
+
+		}
+
 	}
 }
